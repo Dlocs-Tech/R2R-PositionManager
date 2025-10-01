c5240c21b525e873e4e48a163292cb9f6d6af868
Berbex <berbesidaniel@gmail.com>
2025-10-01 11:47:32 -0400

    feat: add info to run actual test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,7 @@
 import PositionManager from "./positionManager.spec";
 import PositionManagerWithExclusiveManager from "./positionManagerWithExclusiveManager.spec";
+import ActualPositionManagerWithExclusiveManager from "./actualPositionManagerWithExclusiveManager.spec";
 
 PositionManager();
 PositionManagerWithExclusiveManager();
+// ActualPositionManagerWithExclusiveManager(); // Run this with blocknumber 63106587
