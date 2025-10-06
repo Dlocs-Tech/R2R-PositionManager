4b2b87a34af48106710321c86994bd3c720ac389
Berbex <berbesidaniel@gmail.com>
2025-10-06 17:27:49 -0400

    feat: add extra comments

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,6 +2,6 @@ import PositionManager from "./positionManager.spec";
 import PositionManagerWithExclusiveManager from "./positionManagerWithExclusiveManager.spec";
 import ActualPositionManagerWithExclusiveManager from "./actualPositionManagerWithExclusiveManager.spec";
 
-PositionManager();
-PositionManagerWithExclusiveManager();
+PositionManager(); // Run this with blocknumber 44364882
+PositionManagerWithExclusiveManager(); // Run this with blocknumber 44364882
 // ActualPositionManagerWithExclusiveManager(); // Run this with blocknumber 63106587
