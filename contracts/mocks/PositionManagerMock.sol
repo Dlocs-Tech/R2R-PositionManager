8edf4fd19cc00eb52e820330e8eba1c8fafe5b8a
Berbex <berbesidaniel@gmail.com>
2025-10-23 20:50:33 -0400

    feat: add deposit function in mock

--- contracts/mocks/PositionManagerMock.sol
+++ contracts/mocks/PositionManagerMock.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IProtocolManager} from "../interfaces/IProtocolManager.sol";
+import {ILocker} from "../interfaces/ILocker.sol";
 
 contract PositionManagerMock is ERC20 {
     constructor() ERC20("PMMock", "PMM") {}
@@ -17,5 +18,11 @@ contract PositionManagerMock is ERC20 {
 
     function registerDeposit(address depositor, IProtocolManager protocolManager) external {
         protocolManager.registerDeposit(depositor);
+    }
+
+    function deposit(IERC20 baseToken, ILocker locker) external {
+        baseToken.approve(address(locker), type(uint256).max);
+
+        locker.deposit(baseToken.balanceOf(address(this)));
     }
 }
